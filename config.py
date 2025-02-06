2a8850c7d37ddc18ffb3b964728a375235b7f69e
Garvit-Mehra <114169456+Garvit-Mehra@users.noreply.github.com>
2025-02-06 22:33:53 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# Configuration paths
-CONFIG_PATH = "model_data/yolov3.cfg"
-WEIGHTS_PATH = "model_data/yolov3.weights"
-CLASSES_PATH = "model_data/coco.names"
-CONFIDENCE_THRESHOLD = 0.5
-NMS_THRESHOLD = 0.4
