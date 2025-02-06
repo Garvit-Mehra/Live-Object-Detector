4a85984bd362dd95581602a62555d06edeee88c0
Garvit Mehra <garvitmehra@MacBook-Pro-2.local>
2025-02-06 22:49:12 +0530

    Added config file

---
+++ config.py
@@ -0,0 +1,6 @@
+# Configuration paths
+CONFIG_PATH = "model_data/yolov3.cfg"
+WEIGHTS_PATH = "model_data/yolov3.weights"
+CLASSES_PATH = "model_data/coco.names"
+CONFIDENCE_THRESHOLD = 0.5
+NMS_THRESHOLD = 0.4
